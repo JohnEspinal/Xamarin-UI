c9f7fd41191eeaabc06ff5476c707bef9dce6218
JohnEspinal <johnmespinalh@gmail.com>
2021-02-19 23:51:43 -0400

    Initial commit

---
+++ Xamarin-UI/Xamarin-UI/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_UI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(LoginUserField.Text))
+            {
+                await DisplayAlert("Error", "User field cannot be empty", "OK");
+            }else if(string.IsNullOrEmpty(LoginPswField.Text))
+            {
+                await DisplayAlert("Error", "Password field cannot be empty", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Welcome!", $"Hello, {LoginUserField.Text}. You are succefully Logged In.", "OK");
+                await Navigation.PushAsync(new HomePage());
+            }
+        }
+
+        private async void SignUpClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+        }
+    }
+}
---
+++ Xamarin-UI/Xamarin-UI/SignUpPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_UI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class SignUpPage : ContentPage
+{
+    public SignUpPage()
+    {
+        InitializeComponent();
+    }
+
+        private async void SignInCompleted_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginUserField.Text) || string.IsNullOrEmpty(LoginPswField.Text) || string.IsNullOrEmpty(LoginPswConfirmField.Text) || string.IsNullOrEmpty(LoginUserNameField.Text))
+            {
+                await DisplayAlert("Error", "No fields can be empty", "OK");
+            }
+            else if (LoginPswField.Text != LoginPswConfirmField.Text)
+            {
+                await DisplayAlert("Error", "The passwords you entered are not the same.", "OK");
+            } else
+            {
+                await DisplayAlert("Welcome!", $"Hello, {LoginUserNameField.Text}. You are succefully Logged In.", "OK");
+                
+                await Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
